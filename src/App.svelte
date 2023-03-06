37eac4fca16b113cf8218a5420a7d6be3b77e4be
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2023-03-06 22:57:04 +0100

    quality of life change
    Fully center preview

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <main>
-	<div class="grid content-center h-[100vh]">
+	<div class="grid place-content-center h-[100vh]">
 		<p class="text-secondary text-center text-9xl font-poppins">App Component</p>
 	</div>
 </main>
