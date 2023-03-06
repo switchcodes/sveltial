eb01a1da9319b90aabd621538a7e7f9cd9e0faa3
HF <f2hafner@edu.aau.at>
2023-03-06 23:16:38 +0100

    merge completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import Button from "./lib/sveltial/input/button/simple.svelte";
-	import SquareBtn from "./lib/sveltial/input/button/square.svelte";
+	import SimpleCard from "./lib/sveltial/framework/card/simple.svelte";
 </script>
 
 <main>
 	<div class="grid place-content-center h-[100vh]">
-		<p class="text-secondary text-center text-9xl font-poppins">App Component</p>
+		<!-- <p class="text-secondary text-center text-9xl font-poppins">App Component</p> -->
+		<SimpleCard />
 	</div>
 </main>
 
---
+++ src/lib/sveltial/framework/card/simple.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="bg-secondary">
+	<slot />
+</div>
