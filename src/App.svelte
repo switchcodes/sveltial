90ee74e267cfa896d581bbe868c5a374073b1248
HF <f2hafner@edu.aau.at>
2023-03-06 21:39:47 +0100

    added square button

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 	import Button from "./lib/sveltial/input/button/simple.svelte";
+	import SquareBtn from "./lib/sveltial/input/button/square.svelte";
 </script>
 
 <main class="font-primary bg-background dark:bg-background-dark">
-	<div class="grid content-center h-[100vh]">
+	<div class="grid place-content-center h-[100vh]">
 		<!-- <p class="text-secondary text-center text-9xl font-poppins">App Component</p> -->
-		<Button
-			><span class="text-secondary align-middle">Label</span>
-			<span class="text-secondary mso-icon align-middle">quiz</span></Button
-		>
+		<Button label="Label" icon="quiz" />
+		<SquareBtn icon="quiz" />
 	</div>
 </main>
 
---
+++ src/lib/sveltial/input/button/square.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let icon: string;
+</script>
+
+<button class="svt-button-style p-2 aspect-square">
+	{#if icon != null}
+		<span class="mso-icon align-middle">{icon}</span>
+	{/if}
+</button>
